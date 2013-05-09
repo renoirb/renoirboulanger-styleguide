7fc27795625349c7303c55131f642a09792c9149
Renoir Boulanger <hello@renoirboulanger.com>
2013-05-08 21:46:48 -0400

    Grunt workspace

--- Gruntfile.js
+++ Gruntfile.js
@@ -232,7 +232,6 @@ module.exports = function (grunt) {
             ],
             dist: [
                 'recess',
-                'svgmin',
                 'htmlmin'
             ]
         },
@@ -250,7 +249,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('server', function (target) {
         if (target === 'dist') {
-            return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+            return grunt.task.run(['build', 'connect:dist:keepalive']);
         }
 
         grunt.task.run([
